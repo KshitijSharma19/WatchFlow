0fa06d5b7a258fe3cadf8b95608e1b595cd4f836
KshitijSharma19 <kshitijsharma1920@gmail.com>
2026-09-14 03:30:14 +0530

    Fix Vercel serverless function dependencies with root package.json

--- api/index.js
+++ api/index.js
@@ -2,6 +2,10 @@ const connectDB = require("../server/config/db");
 const app = require("../server/server");
 
 module.exports = async (req, res) => {
-  await connectDB();
+  try {
+    await connectDB();
+  } catch (err) {
+    console.error("❌ [Vercel Serverless DB Error]", err.message);
+  }
   return app(req, res);
 };
